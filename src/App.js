2bcbd966b2a16b8c0bac3c5db6501b9ea7e6cc90
ak25599 <akshay25599@gmail.com/>
2023-08-12 18:02:02 +0530

    props

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App"> 
-      <User />
+      <User name= "Akshay" age={21} email= "akshay25599@gmail.com" />
       <User />
       <User />
     </div>
   );
 }
-const User = () => {
+
+
+const User = (props) => {
   return (
     <div>
-      <h1> Akshay</h1>
-      <h1> 24</h1>
-      <h1> akshay25599@gmail.com</h1>
+      <h1> {props.name}</h1>
+      <h1> {props.age}</h1>
+      <h1> {props.email}</h1>
     </div>
   )
+
 }
 export default App;
