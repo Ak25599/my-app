c71392fe2f05d8bb6d3d49b3d1550c0b48822b52
ak25599 <akshay25599@gmail.com/>
2023-08-14 10:40:33 +0530

    move

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { User } from './User'
 
 function App() {
   const users = [
     { name:"Akshay", age: 24},
     { name:"Vino", age: 26},
     { name:"Akash", age: 20},
-    { name:"Rubby" age: 20},
+    { name:"Rubby", age: 20},
     ];
 
 return (
 <div className="App">
-  {names.map((name, key) => {
-    return <h1 key={key}> {name} </h1>;
+  {users.map((user, key) => {
+    return <User name={user.name} age={user.age} />;
   })
   }
 </div>
