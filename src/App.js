5ba93cea45b4b730159112d15468b99d6cb966b8
ak25599 <akshay25599@gmail.com/>
2023-08-14 10:39:25 +0530

    operators

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const names = ["Akshay", "Vino", "Akash", "Rubby"];
+  const users = [
+    { name:"Akshay", age: 24},
+    { name:"Vino", age: 26},
+    { name:"Akash", age: 20},
+    { name:"Rubby" age: 20},
+    ];
 
 return (
 <div className="App">
