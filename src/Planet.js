1cd5d231d5984aaff345fc7bb5d6076e10a5939c
ak25599 <akshay25599@gmail.com/>
2023-08-14 10:57:01 +0530

    exampls

---
+++ src/Planet.js
@@ -0,0 +1,6 @@
+export const Planets = [
+    { name:"Mars", isGasPlanet: false },
+    { name:"Earth", agisGasPlanet: false},
+    { name:"Jupiter", agisGasPlanet: true},
+    { name:"Venus", agisGasPlanet: false},
+    ];
