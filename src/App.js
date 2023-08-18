84a40b8837734075a154c4c5e52a59fc8449f366
ak25599 <akshay25599@gmail.com/>
2023-08-18 11:59:23 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ import Axios from "axios";
 
 
 function App() {
+  const [generatedEscuse, setGeneratedExcuse] = useState("")
+
+  const fetchExcuse = (excuse) => {
+    Axios.get('https://excuser-three.vercel.app/v1/excuse/party/').then(
+      (res) => {
+        setGeneratedExcuse(res.data[0].excuse);
+      }
+    );
+  };
   
   return (
     <div className="App">
-      <button> predict Age</button>
-      
+      <h1> Generate An Excuse </h1>
+      <button onClick={() => fetchExcuse("party")}> Party</button>
+      <button onClick={() => fetchExcuse("family")}> Family</button>
+      <button  onClick={() => fetchExcuse("office")}> Office </button>
+       
+
+       <p> {generatedEscuse} </p>
       </div>
 
 
