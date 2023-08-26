c9b6bca3ed7f9b78b01ab50ab55a7e7cb16d0b79
ak25599 <akshay25599@gmail.com/>
2023-08-26 16:31:34 +0530

    hooks

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,19 +1,18 @@
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import Axios from "axios";
+import { useGetCat } from "../useGetCat"; 
 
-const Cat = () => {
-  const { data: catData } = useQuery(["cat"], async () => {
-    const response = await Axios.get("https://catfact.ninja/fact");
-    return response.data;
-  });
+export const Cat = () => {
+  const { data, isCatLoading, refetchData }  = useGetCat();
+
+  if (isCatLoading) return <h1>Loading...</h1>;
 
   return (
     <div>
-      <h1>{catData?.fact}</h1>
+      <button onClick={refetchData}>Refetch</button>
+      <h1>{data?.fact}</h1>
     </div>
   );
 };
 
-export { Cat }; // Export the Cat component as a named export
+
+
 
